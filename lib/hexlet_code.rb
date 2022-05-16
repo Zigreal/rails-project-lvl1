38095b8a98da0b9ce985a96614b4733d1d691ef1
Zigreal <grigoriy.nov1998@gmail.com>
2022-05-16 11:39:04 +0300

    Create form builder
    Allowed to create simple 'input' and 'submit' for form.
    Also added escaping characters to prevent unexpected behavior.

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,18 +1,37 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
-
-# Генерирует пустую форму
+# Генерирует форму
 module HexletCode
   class << self
-    def form_for(_object, url: nil)
-      "<form#{build_action(url)} method=\"post\"></form>"
+    def form_for(object, url: nil)
+      "<form action=\"#{url || "#"}\" method=\"post\">#{yield(FormBuilder.new(object)) if block_given?}</form>"
+    end
+  end
+
+  # Генерирует поля формы
+  class FormBuilder
+    attr_accessor :object, :output
+
+    def initialize(object)
+      @object = object
+      @output = []
     end
 
-    private
+    def input(attr_name, **options)
+      output.tap do |output|
+        output << HexletCode::Tag.build("label", for: attr_name) { attr_name.capitalize }
+        output << if options.delete(:as).to_s == "text"
+                    Tag.build("textarea", cols: 20, rows: 40, name: attr_name) { object.public_send(attr_name) }
+                  else
+                    Tag.build("input", name: attr_name, type: "text", value: object.public_send(attr_name), **options)
+                  end
+      end.join
+    end
 
-    def build_action(url)
-      " action=\"#{url || "#"}\""
+    def submit
+      output.tap do |output|
+        output << Tag.build("input", name: "commit", type: "submit", value: "Save")
+      end.join
     end
   end
 
@@ -20,6 +39,7 @@ module HexletCode
 
   # Генерирует html-тэги
   class Tag
+    autoload :ERB, "erb"
     SINGLE_TAGS =
       %w[!doctype area base br col embed hr img input
          keygen link meta param source track wbr].freeze
@@ -37,7 +57,7 @@ module HexletCode
 
       def build_attributes(attributes)
         attributes.each_with_object([]) do |(k, v), result|
-          result << " #{k}=\"#{v}\""
+          result << ERB::Util.h(" #{k}=\"#{v}\"")
         end.join
       end
     end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,28 +2,65 @@
 
 require_relative "test_helper"
 
+def check_input_with_as_option(user)
+  (HexletCode.form_for(user, url: "/users") do |f|
+    f.input :name
+    f.input :job, as: :text
+  end) == "<form action=\"/users\" method=\"post\">" \
+          "<label for=&quot;name&quot;>Name</label>" \
+          "<input name=&quot;name&quot; type=&quot;text&quot; value=&quot;Zigreal&quot;>" \
+          "<label for=&quot;job&quot;>Job</label>" \
+          "<textarea cols=&quot;20&quot; rows=&quot;40&quot; name=&quot;job&quot;>Elins</textarea>" \
+          "</form>"
+end
+
+def check_missing_input(user)
+  assert_raises NoMethodError do
+    HexletCode.form_for user, url: "/users" do |f|
+      f.input :name
+      f.input :job, as: :text
+      # Поля age у пользователя нет
+      f.input :age
+    end
+  end
+end
+
+def check_submit(user)
+  (HexletCode.form_for(user, url: "/users") do |f|
+    f.input :name
+    f.submit
+  end) == "<form action=\"/users\" method=\"post\">" \
+          "<label for=&quot;name&quot;>Name</label>" \
+          "<input name=&quot;name&quot; type=&quot;text&quot; value=&quot;Zigreal&quot;>" \
+          "<input name=&quot;commit&quot; type=&quot;submit&quot; value=&quot;Save&quot;>" \
+          "</form>"
+end
+
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
+  autoload :HexletCode, "hexlet_code"
+
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
+
+  def test_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert HexletCode::Tag.build("br") == "<br>"
-    assert HexletCode::Tag.build("img", src: "path/to/image") == "<img src=\"path/to/image\">"
-    assert HexletCode::Tag.build("input", type: "submit", value: "Save") == "<input type=\"submit\" value=\"Save\">"
-    assert HexletCode::Tag.build("label") { "Email" } == "<label>Email</label>"
-    assert HexletCode::Tag.build("label", for: "email") { "Email" } == "<label for=\"email\">Email</label>"
-    assert HexletCode::Tag.build("div") == "<div></div>"
+  def test_build
+    assert { HexletCode::Tag.build("br") == "<br>" }
+    assert { HexletCode::Tag.build("img", src: "path/to/image") == "<img src=&quot;path/to/image&quot;>" }
+    assert do
+      HexletCode::Tag.build("input", type: "submit", value: "Save") ==
+        "<input type=&quot;submit&quot; value=&quot;Save&quot;>"
+    end
+    assert { HexletCode::Tag.build("label") { "Email" } == "<label>Email</label>" }
+    assert { HexletCode::Tag.build("label", for: "email") { "Email" } == "<label for=&quot;email&quot;>Email</label>" }
+    assert { HexletCode::Tag.build("div") == "<div></div>" }
   end
 
-  User = Struct.new(:name, :job, keyword_init: true)
   def test_form_for
-    user = User.new(name: "Zigreal")
-    assert HexletCode.form_for(user) == "<form action=\"#\" method=\"post\"></form>"
-  end
-
-  def test_form_for_with_url
-    user = User.new(name: "Zigreal")
-    assert HexletCode.form_for(user, url: "/users") == "<form action=\"/users\" method=\"post\"></form>"
+    user = User.new(name: "Zigreal", job: "Elins", gender: "m")
+    assert { check_input_with_as_option(user) }
+    assert { check_missing_input(user) }
+    assert { check_submit(user) }
   end
 end
